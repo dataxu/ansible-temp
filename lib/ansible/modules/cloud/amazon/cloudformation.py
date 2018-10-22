f0c51c77d13eaee0c5db75373263d6c67399c155
sjambhule <sjambhule@dataxu.com>
2018-10-22 10:13:30 +0530

    fix package name

--- lib/ansible/modules/cloud/amazon/cloudformation.py
+++ lib/ansible/modules/cloud/amazon/cloudformation.py
@@ -3,7 +3,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
-from tagging.validate import tag_validator
+from aws_tagging.validate import tag_validator
 
 __metaclass__ = type
 
